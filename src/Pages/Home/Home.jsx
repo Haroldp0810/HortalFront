874ccff2f41a89c083f996e1d0bc87ba37165185
Haroldp0810 <harold.p2320@gmail.com>
2023-05-09 22:41:11 -0500

    repo

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import Navbar from "../../Components/Navbar/Navbar";
+import "./Home.scss";
+
+const Home = () => {
+    return (
+        <div>
+            <Navbar />
+            <div className="background">
+            </div>
+        </div>
+    )
+}
+export default Home;
